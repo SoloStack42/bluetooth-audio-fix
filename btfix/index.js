d9920a2c5fb39d036ee760a8d4eccdf96f29b0c5
SoloStack <60457002+SoloStack42@users.noreply.github.com>
2025-10-18 22:02:03 -0400

    Update index.js

--- btfix/index.js
+++ btfix/index.js
@@ -1,50 +1,30 @@
-// This is just an example. Actual implementation may require more specific details about Kettu's plugin architecture.
+// PreventHandsfreeMode.js
 
-const { KettuPlugin } = require('kettu');
+// Function to prevent handsfree mode
+function preventHandsfreeMode() {
+    // Override the default behavior of enabling handsfree mode
+    window.addEventListener('beforeunload', (event) => {
+        event.preventDefault();
+        event.returnValue = '';
+    });
 
-class DisableHandsfreeMode extends KettuPlugin {
-  constructor(client) {
-    super(client);
-    this._callStartHandlers = [];
-  }
-
-  enable() {
-    // Listen for voice state updates
-    this.client.on("voiceStateUpdate", (oldState, newState) => {
-      if (!newState.channel?.call && oldState.inCall !== newState.inCall) {
-        // Check if the user is on iOS device.
-        if (this.client.utils.isIOS()) {
-          if (newState.inCall) {
-            this.disableHandsfreeMode(newState);
-          }
+    // Monitor for changes in the audio output settings
+    const originalSetSinkId = window.RTCPeerConnection.prototype.setSinkId;
+    window.RTCPeerConnection.prototype.setSinkId = function(sinkId) {
+        if (sinkId === 'handsfree') {
+            // Prevent setting the sink to handsfree
+            return Promise.resolve();
         }
-      }
-    });
-
-    this.client.on("callStarted", call => {
-      // Store call start handlers
-      this._callStartHandlers.push(call);
-    });
-  }
-
-  disableHandsfreeMode(call) {
-    // Here you should implement the logic to disable hands-free mode.
-    // This would involve finding and manipulating Discord's internal DOM elements, which is not recommended as it may lead to account bans or other issues.
-    console.log("Disabling Hands-Free Mode for call with ID: " + call.callId);
-  }
-
-  disable() {
-    this.client.off("voiceStateUpdate", this.voiceStateUpdate);
-    this.client.off("callStarted", this.callStarted);
-
-    // Remove stored call start handlers
-    while (this._callStartHandlers.length) {
-      const call = this._callStartHandlers.shift();
-      if (!call) return;
-      DiscordClient.delete(call).then(() => console.log(`Deleted ${call}`));
-    }
-  }
-
+        return originalSetSinkId.call(this, sinkId);
+    };
 }
 
-module.exports = DisableHandsfreeMode;
+// Initialize the plugin
+function onPluginStart() {
+    preventHandsfreeMode();
+}
+
+// Export the plugin definition
+module.exports = {
+    onPluginStart
+};
