42016a2112def5a12adb56ca0df21d709cc3d2ea
SoloStack <60457002+SoloStack42@users.noreply.github.com>
2025-10-18 22:15:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-import { ReactNative as RN } from "@vendetta/metro/common";
-import { instead } from "@vendetta/patcher";
+// PreventHandsfreeMode.js
 
-export const onUnload = instead("setCommunicationModeOn", RNReactNative.TurboModuleRegistry.get("NativeAudioManagerModule") === null ? RN.TurboModuleRegistry.get("RTNAudioManager") : RN.TurboModuleRegistry.get("NativeAudioManagerModule"), () => {});
+// Function to prevent handsfree mode
+function preventHandsfreeMode() {
+    // Override the default behavior of enabling handsfree mode
+    window.addEventListener('beforeunload', (event) => {
+        event.preventDefault();
+        event.returnValue = '';
+    });
+
+    // Monitor for changes in the audio output settings
+    const originalSetSinkId = window.RTCPeerConnection.prototype.setSinkId;
+    window.RTCPeerConnection.prototype.setSinkId = function(sinkId) {
+        if (sinkId === 'handsfree') {
+            // Prevent setting the sink to handsfree
+            return Promise.resolve();
+        }
+        return originalSetSinkId.call(this, sinkId);
+    };
+}
+
+// Initialize the plugin
+function onPluginStart() {
+    preventHandsfreeMode();
+}
+
+// Export the plugin definition
+module.exports = {
+    onPluginStart
+};
